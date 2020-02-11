9e5eee0d1d7ecd12ddb8a0d9dae330f9cb2742db
Steven Reiss <sgreiss92@gmail.com>
2020-02-10 22:52:23 -0500

    update url

--- my_lambdata/my_module.py
+++ my_lambdata/my_module.py
@@ -1,2 +1,5 @@
-def enlarge (n):
-    return n*100
+def split_data(X):
+  train, val = train_test_split(X, random_state=42)
+  train = train.copy
+  val = val.copy
+  return train, val
--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
-    name="s2t2-game-utils",
-#     version="1.2",
-#     author="MJ Rossetti",
-#     author_email="datacreativellc@gmail.com",
-#     description="Gameplay logic for Rock-Paper-Scissors",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown", # required if using a md file for long desc
-#     license="MIT",
-#     url="https://github.com/s2t2/game-utils-py",
-#     keywords="rock paper scissors game",
-#     packages=find_packages() # ["game_utils"]
-# )
+    name="sreiss-lambdata-functions",
+    version="0.1",
+    author="Steve Reiss",
+    author_email="sgreiss92@gmail.com",
+    description="functions for assignment",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    license="MIT",
+    url="https://github.com/steve122192/my-lambdata-package",
+    keywords="lambda school assignment",
+    packages=find_packages() # ["lambdata"]
+)
