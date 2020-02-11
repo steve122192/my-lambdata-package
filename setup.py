4a5e3b51c1308f324ebc4c54330f2f20d9807ac4
Steven Reiss <sgreiss92@gmail.com>
2020-02-10 23:46:32 -0500

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
-    name="sreiss-lambdata-functions",
+    name="lambdata-steve122192",
     version="0.1",
     author="Steve Reiss",
     author_email="sgreiss92@gmail.com",
